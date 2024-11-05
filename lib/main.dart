d6572a3ef8c1546a16aa90899c17f846dd3ba065
ailezn <zalia.5220411418@student.uty.ac.id>
2024-11-06 01:11:30 +0700

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:versatile_app/screens/add_screen.dart';
+import 'package:versatile_app/screens/changepassword.dart';
+import 'package:versatile_app/screens/cupboard_screen.dart';
+import 'package:versatile_app/screens/editprofile.dart';
+import 'package:versatile_app/screens/forgotpassword.dart';
+import 'package:versatile_app/screens/home_screen.dart';
+import 'package:versatile_app/screens/loginscreen.dart';
+import 'package:versatile_app/screens/profile_screen.dart';
+import 'package:versatile_app/screens/recommended_screen.dart';
+import 'package:versatile_app/screens/registrationscreen.dart';
 import 'screens/splashscreen.dart';
+
+
 
 void main() {
   runApp(MyApp());
@@ -15,7 +27,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SplashScreenWidget(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreenWidget(),
+        '/register': (context) => const RegistrationWidget(),
+        '/login': (context) => const LoginScreen(),
+        '/forgot-password': (context) => const ForgotpasswordWidget(),
+        '/home': (context) => HomeScreen(),
+        '/recommend': (context) => RecommendedScreen(),
+        '/add': (context) => AddScreen(),
+        '/cupboard': (context) => CupboardScreen(),
+        '/profile': (context) => ProfileWidget(),
+        '/edit-profile': (context) => EditProfileWidget(),
+        '/change-profile': (context) => ChangePasswordWidget(),
+      },
+
     );
   }
 }
